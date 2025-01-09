c671ade31f1897fcc546c8ed6fec0368ef84b1f5
Hồng Yến <63199543+nguyenhongyen@users.noreply.github.com>
2025-01-09 22:44:08 +0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,40 @@
 export function handleViewPassWord() {
-	const btnViewPass = document.querySelector('.view-pass');
-	if(btnViewPass){
-		btnViewPass.addEventListener("click", function() {
-			let elm_id = btnViewPass.getAttribute('data-id');
-			let inputId = document.getElementById(elm_id);
+    const btnViewPass = document.querySelector('.view-pass');
+    if (btnViewPass) {
+        btnViewPass.addEventListener("click", function () {
+            let elm_id = btnViewPass.getAttribute('data-id');
+            let inputId = document.getElementById(elm_id);
 
-			const hasClassShow = btnViewPass.classList.contains('is-show');
-			if (hasClassShow) {
-				btnViewPass.innerHTML = '<i class="fas fa-eye">';
-				btnViewPass.classList.remove('is-show');
-				inputId.setAttribute('type', 'password');
-			} else {
-				btnViewPass.innerHTML = '<i class="fas fa-eye-slash">';
-				btnViewPass.classList.add('is-show');
-				inputId.setAttribute('type', 'text');
-			}
-		})
-	}
+            const hasClassShow = btnViewPass.classList.contains('is-show');
+            if (hasClassShow) {
+                btnViewPass.innerHTML = '<i class="fas fa-eye">';
+                btnViewPass.classList.remove('is-show');
+                inputId.setAttribute('type', 'password');
+            } else {
+                btnViewPass.innerHTML = '<i class="fas fa-eye-slash">';
+                btnViewPass.classList.add('is-show');
+                inputId.setAttribute('type', 'text');
+            }
+        })
+    }
 }
 
+export function handleInitMaterialDate() {
+    let handleInitMaterial = document.querySelectorAll('.handleInitMaterial');
+    if (handleInitMaterial !== null) {
+        handleInitMaterial.forEach(function (item) {
+            const picker = new MaterialDatePicker().on('submit', (val) => {
+                item.value = moment(val.toDate()).format('DD/MM/YYYY');
+            })
+
+            item.addEventListener('click', function () {
+                picker.open()
+            });
+        });
+    }
+}
 
 handleViewPassWord();
+handleInitMaterialDate();
 
 
